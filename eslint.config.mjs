e3fc784b885747387dd3b657fb8315fb14e522d3
Lucky-ESA <github@luckyskills.de>
2026-05-28 23:00:44 +0200

    disabled jsdoc

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,38 @@
 // ioBroker eslint template configuration file for js and ts files
 // Please note that esm or react based modules need additional modules loaded.
-import config from '@iobroker/eslint-config';
+import config from "@iobroker/eslint-config";
 
 export default [
     ...config,
     {
         // specify files to exclude from linting here
         ignores: [
-            '.dev-server/',
-            '.vscode/',
-            '*.test.js',
-            'test/**/*.js',
-            '*.config.mjs',
-            'build',
-            'dist',
-            'admin/words.js',
-            'admin/admin.d.ts',
-            'admin/blockly.js',
-            '**/adapter-config.d.ts',
-            'widgets/**/*.js'
+            ".dev-server/",
+            ".vscode/",
+            "*.test.js",
+            "test/**/*.js",
+            "*.config.mjs",
+            "build",
+            "dist",
+            "admin/words.js",
+            "admin/admin.d.ts",
+            "admin/blockly.js",
+            "**/adapter-config.d.ts",
+            "widgets/**/*.js",
         ],
     },
     {
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
         // as this improves maintainability. jsdoc warnings will not block build process.
         rules: {
+            "jsdoc/reject-any-type": "off",
+            "jsdoc/reject-function-type": "off",
+            "@typescript-eslint/ban-ts-comment": "off",
+            "jsdoc/check-tag-names": "off",
             // 'jsdoc/require-jsdoc': 'off',
             // 'jsdoc/require-param': 'off',
-            // 'jsdoc/require-param-description': 'off',
-            // 'jsdoc/require-returns-description': 'off',
+            "jsdoc/require-param-description": "off",
+            "jsdoc/require-returns-description": "off",
             // 'jsdoc/require-returns-check': 'off',
         },
     },
